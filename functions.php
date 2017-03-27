58c018ff0eb3559e1844009e02f8256d87aed5b7
Navin Sridhar <navinsridhar95@gmail.com>
2017-03-27 22:16:45 +0530

    Enqueue jQuery and CSS

--- functions.php
+++ functions.php
@@ -10,7 +10,14 @@
  */
 function wdm_bootstrap_scripts() {
 	// Theme stylesheet.
-	wp_enqueue_style( 'wdm-bootstrap-custom-style', get_stylesheet_uri() );
-	wp_enqueue_style('bootstrap-style', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css');
+	wp_register_style( 'wdm-bootstrap-custom-style', get_stylesheet_uri(), array(), null, 'all' );
+	wp_enqueue_style( 'wdm-bootstrap-custom-style' );
+	wp_register_style('bootstrap-style', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css', array(), null, 'all' );
+	wp_enqueue_style( 'bootstrap-style' );
+	wp_deregister_script( 'jquery' );
+	wp_register_script( 'jquery', '//code.jquery.com/jquery-3.2.1.min.js', array(), null, false );
+	wp_enqueue_script( 'jquery' );
+	wp_register_script( 'bootstrap-jquery', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js', array('jquery'), null, true );
+	wp_enqueue_script( 'bootstrap-jquery' );
 }
 add_action( 'wp_enqueue_scripts', 'wdm_bootstrap_scripts' );
