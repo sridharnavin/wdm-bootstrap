2e7dbab258dd59e2a68ea6ea222b9fed549a64e3
Navin Sridhar <navinsridhar95@gmail.com>
2017-03-27 22:17:14 +0530

    Add navbar to header

--- header.php
+++ header.php
@@ -16,9 +16,30 @@
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+
+	<!-- site-header -->
+	<header class="container site-header">
+		<h1 class="header-title"><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+		<h5><?php bloginfo( 'description' ); ?></h5>
+	</header><!-- site-header -->
+
+	<!-- Fixed navbar -->
+	<nav class="navbar navbar-default">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+		            <span class="sr-only">Toggle navigation</span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">WDM Bootstrap</a>
+			</div>
+			<div id="navbar" class="navbar-collapse collapse">
+				<ul class="nav navbar-nav">
+		            <li class="active"><a href="<?php echo home_url(); ?>">Home</a></li>
+	    		</ul>
+			</div><!--/.nav-collapse -->
+		</div>
+	</nav>
 	<div class="container">
-		<!-- site-header -->
-		<header class="site-header">
-			<h1><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-			<h5><?php bloginfo( 'description' ); ?></h5>
-		</header><!-- site-header -->
